e043d85b638d30f3d0640b50ef7ca92dd32a854f
Baldev Sahu <bsahu@codeaurora.org>
2017-12-21 10:41:59 +0530

    sdm660-common: Disable debug.sf.recomputecrop
    Disable recompute crop change in SF
    
    Change-Id: Ifd5bae734ccc4962427a64f789cf9890f10f5965

--- properties.mk
+++ properties.mk
@@ -125,6 +125,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=0 \
+    debug.sf.recomputecrop=0 \
     vendor.gralloc.enable_fb_ubwc=1 \
     debug.sf.disable_backpressure=1 \
     dev.pm.dyn_samplingrate=1 \
