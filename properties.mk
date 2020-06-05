703987a4d06ad64ae220d8f6e358eba402237384
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2020-06-05 11:57:33 -0400

    sdm660-common: Set debug.sf.enable_hwc_vds property to false.
    * This helps fix the green screen issue during screen recording.
    
    Change-Id: I37b17a2ede9505515adb793066a7d440708e5684

--- properties.mk
+++ properties.mk
@@ -120,7 +120,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.hwc_set_default_colormode=true \
-    debug.sf.enable_hwc_vds=1 \
+    debug.sf.enable_hwc_vds=0 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
