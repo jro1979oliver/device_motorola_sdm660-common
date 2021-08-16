evaxbot/sdm660-common

sdm660-common: Introduce kernelspace battery saver
 * Needs Kernel side support
 * This activates kernelspace battery saver via powerhal whenever
   battery saver is enabled in userspace, thereby lowering power
   consumption at kernel-level by disabling boosts and such

Signed-off-by: clarencelol <clarencekuiek@icloud.com>

BoardConfigCommon.mk
@@ -79,6 +79,9 @@ BOARD_SUPPORTS_SOUND_TRIGGER := true
 USE_CUSTOM_AUDIO_POLICY := 1
 USE_XML_AUDIO_POLICY_CONF := 1
 
+# Battery Saver
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power-libperfmgr/power/power-mode.cpp
+
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true

jro1979oliver/device_motorola_sdm660-common

Change-Id: I5c37af9d4457de665c6b6d38776a4ed4d8fb1181

BoardConfigCommon.mk
@@ -54,6 +54,9 @@ BOARD_SUPPORTS_SOUND_TRIGGER := true
 BOARD_USES_ALSA_AUDIO := true
 USE_CUSTOM_AUDIO_POLICY := 1
 
+# Battery Saver
+TARGET_POWERHAL_MODE_EXT := $(PLATFORM_PATH)/power-libperfmgr/power/power-mode.cpp
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(PLATFORM_PATH)/bluetooth
 BOARD_HAVE_BLUETOOTH := true