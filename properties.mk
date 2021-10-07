51db3e9175c38f1a5ea61ee35c3b14262182ff27
Arian <arian.kulmer@web.de>
2021-10-07 13:29:32 +0200

    sdm660-common: Add soc properties
    Change-Id: Ib081ffc6e052b0165223c4a53b4d709a9c0a5fab

--- properties.mk
+++ properties.mk
@@ -240,6 +240,11 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     renderthread.skia.reduceopstasksplitting=true
 
+# SOC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.soc.manufacturer=Qualcomm \
+    ro.soc.model=SDM660
+
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
